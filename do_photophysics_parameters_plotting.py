309ed08355cd8c8bc8b819a3861097ef4896b198
abdelneuhaus <abdelghani.neuhaus@u-bordeaux.fr>
2023-03-13 14:39:44 +0100

    fix bug when photophysics plots were identical for all conditions

--- do_photophysics_parameters_plotting.py
+++ do_photophysics_parameters_plotting.py
@@ -6,8 +6,6 @@ import csv
 import os
 import statistics
 import math
-import numpy as np
-from scipy.stats import norm
 
 def lire_csv(nom_fichier):
     lignes = []
@@ -70,13 +68,11 @@ def photon_calculation(liste, sigma=1):
         exp_liste.append(int(valeur/(math.sqrt(2*math.pi))))
     return exp_liste
 
-
 def loc_prec_calculation(sigma, photon_loc):
     otp = []
     for i in range(len(sigma)):
         otp.append(float(sigma[i]/(math.sqrt(photon_loc[i]))))
     return otp    
-
 
 # Define helper function
 def get_num_fov_idx_results_dir(i, exp, PT_561, PT_405):
@@ -108,20 +104,18 @@ def do_photophysics_parameters_plotting(list_of_poca_files, list_of_frame_csv, l
                                         on_times=True, off_times=True, total_on=True, num_blinks=True,
                                         phot_per_loc=True, phot_per_cluster=True, num_on_times=True, 
                                         num_off_times=True, sigma=True, drop_one_event=False, boxplot=True):
-    for j in list_of_frame_csv:
-        # length of each ON time
-        _on_times = pre_process_on_frame_csv(j, on_filter=drop_one_event) if on_times else None
-        # length of each OFF time
-        _off_times = pre_process_off_frame_csv(j, on_filter=drop_one_event) if off_times else None
-    
     tmp_pho_loc = list()
-    for k in list_of_int_csv:
+    cpt = 0
+    for j in range(len(list_of_frame_csv)):
+        # length of each ON time
+        _on_times = pre_process_on_frame_csv(list_of_frame_csv[j], on_filter=drop_one_event) if on_times else None
+        # length of each OFF time
+        _off_times = pre_process_off_frame_csv(list_of_frame_csv[j], on_filter=drop_one_event) if off_times else None
         # photon per localization
-        _phot_per_loc = photon_calculation(pre_process_single_intensity(k, on_filter=drop_one_event)) if phot_per_loc else None
-        tmp_pho_loc.append(photon_calculation(pre_process_single_intensity(k, on_filter=drop_one_event)))
+        _phot_per_loc = photon_calculation(pre_process_single_intensity(list_of_int_csv[j], on_filter=drop_one_event)) if phot_per_loc else None
+        tmp_pho_loc.append(photon_calculation(pre_process_single_intensity(list_of_int_csv[j], on_filter=drop_one_event)))
         
-    for i in list_of_poca_files:
-        raw_file_poca = read_poca_files(i)
+        raw_file_poca = read_poca_files(list_of_poca_files[j])
         if drop_one_event == True:
             init = len(raw_file_poca)
             raw_file_poca = raw_file_poca[raw_file_poca['total ON'] > 1]
@@ -137,15 +131,9 @@ def do_photophysics_parameters_plotting(list_of_poca_files, list_of_frame_csv, l
         _num_on_times = raw_file_poca.loc[:, '# seq ON'].values.tolist() if num_on_times else None
         # number of OFF times per cluster
         _num_off_times = raw_file_poca.loc[:, '# seq OFF'].values.tolist() if num_off_times else None
-
-    cpt = 0
-    for h in list_of_sigma_csv:
-        _sigma = loc_prec_calculation(pre_process_sigma(h, on_filter=drop_one_event), tmp_pho_loc[cpt]) if sigma else None
+        _sigma = loc_prec_calculation(pre_process_sigma(list_of_sigma_csv[j], on_filter=drop_one_event), tmp_pho_loc[cpt]) if sigma else None
         cpt += 1
         
-
-    for i in list_of_poca_files:
-        # imprime les éléments qui ne sont pas égaux à None
         non_none_elements = [_on_times, _off_times, _total_on, _num_blinks, _phot_per_loc, _phot_per_cluster, _num_on_times, _num_off_times, _sigma]
         non_none_elements = [elem for elem in non_none_elements if elem is not None]
         label = ['ON times', "OFF times", "Total ON (Bleachtime)", "#Blinks", "Photon/loc", "Photon/cluster", "#ON times", "#OFF times", "Loc. Precision"]
@@ -167,14 +155,14 @@ def do_photophysics_parameters_plotting(list_of_poca_files, list_of_frame_csv, l
 
             
         # Si pas PT experiment (pas de 561-405) et que le nom du PT est le nom du dossier (dossier -> dossier.PT)
-        title_plot = os.path.basename(os.path.normpath(i.replace('.PT/locPALMTracer_cleaned.txt', '')))
+        title_plot = os.path.basename(os.path.normpath(list_of_poca_files[j].replace('.PT/locPALMTracer_cleaned.txt', '')))
         results_dir = os.path.join('results/'+exp+'/'+title_plot+'/')
         plt.suptitle(title_plot, fontsize=14)
 
         if isPT == True:
             PT_405 = '/561-405.PT/locPALMTracer_cleaned.txt'
             PT_561 = '/561.PT/locPALMTracer_cleaned.txt'
-            results_dir, title_plot = get_num_fov_idx_results_dir(i, exp, PT_561, PT_405)
+            results_dir, title_plot = get_num_fov_idx_results_dir(list_of_poca_files[j], exp, PT_561, PT_405)
             plt.suptitle(title_plot, fontsize=14)
         
         sample_file = '/photophysics_plots.pdf'
